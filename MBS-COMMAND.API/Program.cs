7aa4145a23bcff28df82003e5f4cfa5d156b95f6
bentran1vn <tan182205@gmail.com>
2024-09-16 20:23:28 +0700

    Config Carter

--- MBS-COMMAND.API/Program.cs
+++ MBS-COMMAND.API/Program.cs
@@ -1,25 +1,61 @@
+using Antree_Ecommerce_BE.API.DependencyInjection.Extensions;
+using Carter;
 using MBS_COMMAND.API.DependencyInjection.Extensions;
+using MicroElements.Swashbuckle.FluentValidation.AspNetCore;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+Log.Logger = new LoggerConfiguration().ReadFrom
+    .Configuration(builder.Configuration)
+    .CreateLogger();
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Logging
+    .ClearProviders()
+    .AddSerilog();
+
+builder.Host.UseSerilog();
+
+// Add Carter module
+builder.Services.AddCarter();
+
+builder.Services
+    .AddSwaggerGenNewtonsoftSupport()
+    .AddFluentValidationRulesToSwagger()
+    .AddEndpointsApiExplorer()
+    .AddSwaggerAPI();
+
+builder.Services
+    .AddApiVersioning(options => options.ReportApiVersions = true)
+    .AddApiExplorer(options =>
+    {
+        options.GroupNameFormat = "'v'VVV";
+        options.SubstituteApiVersionInUrl = true;
+    });
+
+builder.Services.ConfigureCors();
+
+// builder.Services.AddMediatRApplication();
+// builder.Services.AddAutoMapperApplication();
 
 builder.Services.AddJwtAuthenticationAPI(builder.Configuration);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+// Configure the HTTP request pipeline. 
+// if (builder.Environment.IsDevelopment() || builder.Environment.IsStaging())
+app.UseSwaggerAPI(); // => After MapCarter => Show Version
+
+app.UseCors("CorsPolicy");
+
+// app.UseHttpsRedirection();
+
+app.UseAuthentication(); // Need to be before app.UseAuthorization();
+app.UseAuthorization();
+
+
+// Add API Endpoint with carter module
+app.MapCarter();
 
 try
 {
