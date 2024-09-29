27d920326e89294bd4db7eb2830019773d2dd3cc
Phuc Nghi <meocodien1762003@gmail.com>
2024-09-29 18:23:20 +0700

    Update DB connection, User entity, and schema
    - Updated connection string in `appsettings.Development.json` to new server and database.
    - Modified `User.cs`: removed `Gender` and `Phonenumber`, added `IsFirstLogin`.
    - Updated `ApplicationDbContextModelSnapshot.cs` to reflect entity changes.
    - Added log entries in `log-20240929.txt` for sensitive data logging.
    - Added migration script `20240929111824_fix_entity.cs` for schema changes.
    - Generated `20240929111824_fix_entity.Designer.cs` to update schema.

--- MBS_COMMAND.Domain/Entities/User.cs
+++ MBS_COMMAND.Domain/Entities/User.cs
@@ -7,12 +7,11 @@ public class User : Entity<Guid>, IAuditableEntity
     public string Email { get; set; }
     public string? FullName { get; set; }
     public string Password { get; set; }
-    public bool Gender { get; set; }
-    public string Phonenumber { get; set; }
     public int Role { get; set; }
     public int Points { get; set; }
     public int Status { get; set; }
     public Guid? MentorId { get; set; }
+    public bool IsFirstLogin { get; set; } = true;
 
     public virtual User? Mentor { get; set; }
     public DateTimeOffset CreatedOnUtc { get; set; }
---
+++ MBS_COMMAND.Persistence/Migrations/20240929111824_fix_entity.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MBS_COMMAND.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class fix_entity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Phonenumber",
+                table: "Users");
+
+            migrationBuilder.RenameColumn(
+                name: "Gender",
+                table: "Users",
+                newName: "IsFirstLogin");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "IsFirstLogin",
+                table: "Users",
+                newName: "Gender");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Phonenumber",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
