6d02ebad500232985be46c18aa9dd653f1686484
Phuc Nghi <meocodien1762003@gmail.com>
2024-10-09 01:30:47 +0700

    Refactor group creation and update entity models
    Updated CreateGroupCommandHandler.cs to include a new repository for User entities, allowing mentor existence checks before group creation. Modified group creation logic to use a new Group object initializer.
    
    Refactored Group.cs to inherit from Entity instead of AggregateRoot, made property setters public, and removed the constructor and Create method.
    
    Added a comment in User.cs to explain possible values for the Status property.

--- MBS_COMMAND.Domain/Entities/Group.cs
+++ MBS_COMMAND.Domain/Entities/Group.cs
@@ -4,35 +4,20 @@ using MBS_COMMAND.Domain.Abstractions.Entities;
 
 namespace MBS_COMMAND.Domain.Entities;
 
-public class Group : AggregateRoot<Guid>, IAuditableEntity
+public class Group : Entity<Guid>, IAuditableEntity
 {
-    public string Name { get; private set; }
-    public Guid? MentorId { get; private set; }
+    public string Name { get;  set; }
+    public Guid? MentorId { get;  set; }
     public virtual User? Mentor { get; set; }
 
     public Guid? LeaderId { get; set; }
     public virtual User? Leader { get; set; }
-    public string Stack { get; private set; }
-    public Guid? ProjectId { get; private set; }
+    public string Stack { get;  set; }
+    public Guid? ProjectId { get;  set; }
     public virtual Project? Project { get; set; }
     public DateTimeOffset CreatedOnUtc { get; set; }
     public DateTimeOffset? ModifiedOnUtc { get; set; }
-
-
-
-    public Group(string name, string stack, Guid? mentorId)
-    {
-        Name = name;
-        Stack = stack;
-        MentorId = mentorId;
-    }
-
-    public static Group Create(string name, string stack, Guid? mentorId)
-    {
-        var G = new Group(name, stack, mentorId);
-        G.RaiseDomainEvent(new Contract.Services.Groups.DomainEvents.GroupCreated(Guid.NewGuid(), G.Id, G.Name, G.MentorId, G.Stack));
-        return G;
-    }
+    
 
 }
 
--- MBS_COMMAND.Domain/Entities/User.cs
+++ MBS_COMMAND.Domain/Entities/User.cs
@@ -9,7 +9,7 @@ public class User : Entity<Guid>, IAuditableEntity
     public string Password { get; set; }
     public int Role { get; set; }
     public int Points { get; set; }
-    public int Status { get; set; }
+    public int Status { get; set; } //0 Not Active, 1 Active, 2 Blocked
     public Guid? MentorId { get; set; }
     public bool IsFirstLogin { get; set; } = true;
 
