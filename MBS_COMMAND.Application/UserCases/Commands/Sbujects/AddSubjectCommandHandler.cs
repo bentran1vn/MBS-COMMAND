6ed5a1aab497bc986e1a0d16eed4c5f1ee0b3862
Eric <36512385+PhucNghi176@users.noreply.github.com>
2024-10-17 17:51:22 +0700

    Add feature to add subjects to the system
    Introduced a new feature for adding subjects to the system:
    - Added `AddSubjectCommandHandler` class to handle the command for adding subjects.
    - Implemented `Handle` method to check for existing subjects and add new ones.
    - Added `Command.AddSubject` record to encapsulate data for adding subjects.
    - Created `SubjectApi` class to define API routes and handle HTTP POST requests for adding subjects.

---
+++ MBS_COMMAND.Application/UserCases/Commands/Sbujects/AddSubjectCommandHandler.cs
@@ -0,0 +1,36 @@
+﻿using MBS_COMMAND.Contract.Abstractions.Messages;
+using MBS_COMMAND.Contract.Abstractions.Shared;
+using MBS_COMMAND.Contract.Services.Subjects;
+using MBS_COMMAND.Domain.Abstractions;
+using MBS_COMMAND.Domain.Abstractions.Repositories;
+using MBS_COMMAND.Domain.Entities;
+
+namespace MBS_COMMAND.Application.UserCases.Commands.Sbujects;
+
+public class AddSubjectCommandHandler(
+    IRepositoryBase<Subject, Guid> subjectRepository,
+    IRepositoryBase<Semester, Guid> semesterRepository,
+    IUnitOfWork unitOfWork)
+    : ICommandHandler<Command.AddSubject>
+{
+
+    public async Task<Result> Handle(Command.AddSubject request, CancellationToken cancellationToken)
+    {
+        var currentSemester = await semesterRepository.FindSingleAsync(x => x.IsActive, cancellationToken);
+        if (currentSemester == null)
+            return Result.Failure(new Error("404", "Semester Not Found"));
+        var isCurrentSubjectExist = subjectRepository.FindAll(x => x.Name == request.Name && x.SemesterId == currentSemester.Id);
+        if (isCurrentSubjectExist.Any())
+            return Result.Failure(new Error("404", "Subject Already Exist In This Semester"));
+        var subject = new Subject
+        {
+            Id = Guid.NewGuid(),
+            Name = request.Name,
+            Status = 1,
+            SemesterId = currentSemester.Id
+        };
+        subjectRepository.Add(subject);
+        await unitOfWork.SaveChangesAsync(cancellationToken);
+        return Result.Success();
+    }
+}
