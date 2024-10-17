8ee02b0f8109c5ed618d1595167dcc2b7d51e8da
Eric <36512385+PhucNghi176@users.noreply.github.com>
2024-10-17 19:14:09 +0700

    hot fix

--- MBS_COMMAND.Application/UserCases/Commands/Groups/CreateGroupCommandHandler.cs
+++ MBS_COMMAND.Application/UserCases/Commands/Groups/CreateGroupCommandHandler.cs
@@ -6,25 +6,24 @@ using MBS_COMMAND.Domain.Entities;
 
 namespace MBS_COMMAND.Application.UserCases.Commands.Groups;
 
-public sealed class CreateGroupCommandHandler(IRepositoryBase<Group, Guid> repositoryBase, IRepositoryBase<User, Guid> userRepository, ICurrentUserService currentUserService) : ICommandHandler<Command.CreateGroupCommand>
+public sealed class CreateGroupCommandHandler(
+    IRepositoryBase<Group, Guid> repositoryBase,
+    IRepositoryBase<User, Guid> userRepository,
+    ICurrentUserService currentUserService) : ICommandHandler<Command.CreateGroupCommand>
 {
     public async Task<Result> Handle(Command.CreateGroupCommand request, CancellationToken cancellationToken)
     {
-        var M = await userRepository.FindSingleAsync(x => x.Id == request.MentorId, cancellationToken);
-        if (M == null)
-            return Result.Failure(new Error("404", "Mentor Not Found"));
-       /* var T = Guid.TryParse(currentUserService.UserId, out Guid L);
+        var T = Guid.TryParse(currentUserService.UserId, out var L);
         if (!T)
-            return Result.Failure(new Error("404", "User Not Authorized"));*/
+            return Result.Failure(new Error("404", "User Not Authorized"));
         var G = new Group
         {
             Name = request.Name,
             MentorId = request.MentorId,
             Stack = request.Stacks,
-           // LeaderId = L
-
+            // LeaderId = L
         };
-        G.Members!.Add(new Group_Student_Mapping { StudentId = M.Id, GroupId = G.Id });
+        G.Members!.Add(new Group_Student_Mapping { StudentId = L, GroupId = G.Id });
         repositoryBase.Add(G);
 
         return Result.Success();
