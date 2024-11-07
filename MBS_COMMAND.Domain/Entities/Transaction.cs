3e99473eff244f8448018e0c3abfb1142063593b
Eric <36512385+PhucNghi176@users.noreply.github.com>
2024-11-07 16:26:25 +0700

    Update Transaction.cs

--- MBS_COMMAND.Domain/Entities/Transaction.cs
+++ MBS_COMMAND.Domain/Entities/Transaction.cs
@@ -1,4 +1,4 @@
-﻿using MBS_COMMAND.Domain.Abstractions.Entities;
+using MBS_COMMAND.Domain.Abstractions.Entities;
 
 namespace MBS_COMMAND.Domain.Entities;
 
@@ -7,7 +7,7 @@ public class Transaction : Entity<Guid>, IAuditableEntity
     public Guid? UserId { get; set; }
     public virtual User? User { get; set; }
     public Guid? ScheduleId { get; set; }
-    public virtual Slot? Schedule { get; set; }
+    public virtual Schedule? Schedule { get; set; }
     public DateOnly Date { get; set; }
     public int Point { get; set; }
     public int Status { get; set; }
